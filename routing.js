f867dd4641cdf6170a6612ec2116b2442b38cc93
vmorantee <msmarzewski7@gmail.com>
2021-12-05 22:31:46 +0100

    white 2115 logowanie rejestracja

--- routing.js
+++ routing.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const router = express.Router()
 const bodyParser = require('body-parser');
-const { json } = require('body-parser');
+const {json} = require('body-parser');
 const knex = require('knex')({
     client: 'mysql',
     connection: {
-        host : '127.0.0.1',
-        port : 3306,
-        user : 'root',
-        password : '',
-        database : 'sklep'
+        host: '127.0.0.1',
+        port: 3306,
+        user: 'root',
+        password: '',
+        database: 'sklep'
     }
 });
 const cookieParser = require("cookie-parser");
 const sessions = require('express-session');
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.urlencoded({extended: false})
 
 var session;
 
@@ -37,22 +37,26 @@ router.post('/rejestracja', urlencodedParser, async (req, res) => {
 
     let email = req.body.email;
     let pass;
-    let response=true //Promise na sprawdzenie maila w bazie
-    await knex.select('mail').table('uzytkownik').where('mail','like',email).then(rows=>{
-        rows.forEach(row => {if(row.mail==email) {response=false}})
+    let response = true //Promise na sprawdzenie maila w bazie
+    await knex.select('mail').table('uzytkownik').where('mail', 'like', email).then(rows => {
+        rows.forEach(row => {
+            if (row.mail == email) {
+                response = false
+            }
+        })
     })
     if (req.body.pass[0] != req.body.pass[1]) {
-        res.render('Rejestracja', { error: 'Podane hasła nie sa takie same!' });
+        res.render('Rejestracja', {error: 'Podane hasła nie sa takie same!'});
 
     } else {
         pass = req.body.pass[0];
     }
 
     if (!response) { // SQL jesli user jest w bazie
-        res.render('Rejestracja', { error: 'Podany email jest już zajety' });
+        res.render('Rejestracja', {error: 'Podany email jest już zajety'});
 
     } else {
-        knex('uzytkownik').insert({typ:'normalny',koszyk:'tak',haslo:pass,mail:email}).then( function (result) {
+        knex('uzytkownik').insert({typ: 'normalny', koszyk: 'tak', haslo: pass, mail: email}).then(function (result) {
         })
         res.render('Logowanie');
 
@@ -78,15 +82,17 @@ router.post('/logowanie', urlencodedParser, async (req, res) => {
     } else {
         let email = req.body.email;
         let pass = req.body.pass;
-
-        req.session.userid = email
-
-        if (true) { //SQL sprawdzenie czy dane logowania sa poprawne $email $pass
-            res.render('strona_glowna');
-
-        } else {
-            res.render('Logowanie', { error: 'Login lub hasło są nieprawidłowe' });
-        }
+        let loginres
+        await knex.select('mail', 'haslo').table('uzytkownik').where('mail', 'like', email).then(rows => {
+            rows.forEach(row => {
+                if (row.mail == email && pass == row.haslo) {
+                    req.session.userid = email
+                    res.render('strona_glowna')
+                } else {
+                    res.render('Logowanie', {error: 'Login lub hasło są nieprawidłowe'});
+                }
+            })
+        })
     }
 
 
