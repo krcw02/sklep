3891b89a0bb20fa4e15279e433a15a6b777b611c
krcw02 <krcw02@gmail.com>
2021-12-05 01:22:44 +0000

    Logowanie

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 const express = require('express');
+const session = require('express-session')
 const path = require('path');
-
-const port = process.env.PORT || 3000
+const bodyParser = require('body-parser');
+const { json } = require('body-parser');
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
+const port = process.env.PORT || 2000
 const app = express()
 app.set('view engine', 'hbs')
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 const publicPath = path.resolve(__dirname, "public");
+
+const oneDay = 1000 * 60 * 60 * 24;
+
+app.use(sessions({
+  secret: "lhjksdbfjebacczarnychawdawndk",
+  saveUninitialized:true,
+  cookie: { maxAge: oneDay },
+  resave: false
+}));
+
+const routing = require('./routing');
+app.use('/', routing);
 
 app.use(express.static(publicPath));
 
-app.get('/', function (req, res) {
-  res.render('index')
-})
- 
+
 app.listen(port)
---
+++ routing.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const router = express.Router()
+const bodyParser = require('body-parser');
+const { json } = require('body-parser');
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+var session;
+
+router.get('/', function (req, res) {
+    res.render('index')
+})
+
+router.get('/rejestracja', function (req, res) {
+    if(req.session.userid){
+        res.render('/strona_glowna');
+    }
+    
+    res.render('Rejestracja');
+})
+
+router.post('/rejestracja', urlencodedParser, async (req, res) => {
+    if(req.session.userid){
+        res.render('/strona_glowna');
+    }
+    
+    let email = req.body.email;
+    let pass;
+    if (req.body.pass[0] != req.body.pass[1]) {
+        res.render('Rejestracja', { error: 'Podane hasła nie sa takie same!' });
+
+    } else {
+        pass = req.body.pass[0];
+    }
+
+    if (true) { // SQL jesli user jest w bazie
+        res.render('Rejestracja', { error: 'Podany email jest już zajety' });
+
+    } else {
+        // SQL wpisanie usera do bazy $email $pass
+        res.render('/strona_glowna');
+
+    }
+
+
+});
+
+router.get('/logowanie', function (req, res) {
+    if(req.session.userid){
+        res.render('/strona_glowna');
+    }
+    res.render('Logowanie');
+})
+
+router.post('/logowanie', urlencodedParser, async (req, res) => {
+    if(req.session.userid){
+        res.render('/strona_glowna');
+    }
+    let email = req.body.email;
+    let pass = req.body.pass;
+
+    if (true) { //SQL sprawdzenie czy dane logowania sa poprawne $email $pass
+        res.render('strona_glowna');
+        req.session.userid = email
+    } else {
+        res.render('Logowanie', { error: 'Login lub hasło są nieprawidłowe' });
+    }
+
+
+
+});
+
+
+module.exports = router
